abeab4197092154f1672ffd4673362c2cb456d60
nfleclerc <nfleclerc@live.com>
2015-11-15 14:24:23 -0500

    added store class
    Signed-off-by: nfleclerc <nfleclerc@live.com>

---
+++ store.java
@@ -0,0 +1,20 @@
+class Store {
+
+    //private constructor
+
+    public void buyAgent(){
+    }
+
+    public void buySupervisor(){
+    }
+
+    public void buyCounter(){
+    }
+
+    public void buyDiamonds(){
+    }
+
+    private void removeCurrency(){
+    }
+
+}
