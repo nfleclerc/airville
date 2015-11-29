9d3e9040cc765344cdfac172fd4ceff89a8447f6
Nathaniel Leclerc <nfleclerc@live.com>
2015-11-28 23:19:36 -0500

    added passenger test

---
+++ src/airville/tests/PassengerTest.java
@@ -0,0 +1,16 @@
+package airville.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nathaniel on 11/28/15.
+ */
+public class PassengerTest {
+
+    @Test
+    public void testProcessAt() throws Exception {
+
+    }
+}
